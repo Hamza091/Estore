63359069989c33fb2e5d7d42a4f979eb6c0d6fd7
Hamza <hamza01961@gmail.com>
2021-09-02 17:43:10 +0500

    done

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var app = express()
 var jsonParser = bodyParser.json()
 app.use(cors())
 console.log("hellow")
-app.use(express.static('client/build'));
+app.use(express.static('./client/build'));
 app.use('/api',jsonParser,Data)
 app.get('*',(req,res)=>{
     res.sendFile(path.resolve(__dirname,'client','build','index.html'));
